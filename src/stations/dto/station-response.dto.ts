1ca7192c44bec8d13a4ae254825e5562d12d0f79
Santiago3110 <s.celisr2@uniandes.edu.co>
2025-10-03 17:26:06 -0500

    SC: Station response dto created

---
+++ src/stations/dto/station-response.dto.ts
@@ -0,0 +1,10 @@
+export class StationResponseDto {
+  id: string;
+  placeName: string;
+  description: string;
+  latitude: number;
+  longitude: number;
+  distanceMeters: number;
+  availableUmbrellas: number;
+  totalUmbrellas: number;
+}
