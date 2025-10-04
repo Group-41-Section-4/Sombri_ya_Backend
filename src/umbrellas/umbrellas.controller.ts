434688a9639e382270baf9d98c43debb26bc1234
gabrielasolerr <g.soler@uniandes.edu.co>
2025-10-03 19:24:34 -0500

    Umbrellas in back

--- src/umbrellas/umbrellas.controller.ts
+++ src/umbrellas/umbrellas.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Umbrella } from 'src/database/entities/umbrella.entity';
 
 @Controller('umbrellas')
-export class UmbrellasController {}
+export class UmbrellasController {
+  constructor(
+    @InjectRepository(Umbrella)
+    private readonly umbrellaRepository: Repository<Umbrella>,
+  ) {}
+
+  @Get()
+  async findAll() {
+    return this.umbrellaRepository.find({
+      relations: ['station'],
+    });
+  }
+}
